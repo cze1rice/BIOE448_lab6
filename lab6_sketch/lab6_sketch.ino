9cac52fe2d7dfaca222222a70ac701720234251b
cze1rice <158530788+cze1rice@users.noreply.github.com>
2025-02-26 14:34:06 -0600

    initial flag and threshold setting

--- lab6_sketch/lab6_sketch.ino
+++ lab6_sketch/lab6_sketch.ino
@@ -1,6 +1,16 @@
 const int sensor_pin = 0;
 int pulse_signal = 0;
+int pulse_period = 0;
 float BPM = 0;
+
+// initialize flags
+bool any_peak_detected = false;
+bool first_peak_detected = false;
+unsigned long first_pulse_time = 0;
+unsigned long second_pulse_time = 0;
+
+int upper_threshold = 900;
+int lower_threshold = 200;
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,5 +20,22 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   pulse_signal = analogRead(sensor_pin);
-  Serial.println(pulse_signal);
+  //Serial.println(pulse_signal);
+  delay(100);
+  if (pulse_signal > upper_threshold && any_peak_detected == false) {
+    any_peak_detected = true;
+    if (first_peak_detected == false) {
+      first_pulse_time = millis();
+      first_peak_detected == true;
+    }
+    else {
+      second_pulse_time = millis();
+      pulse_period = second_pulse_time - first_pulse_time;
+      Serial.println(pulse_period);
+      first_peak_detected = false;
+    }
+  }
+  if (pulse_signal < lower_threshold) {
+    any_peak_detected = false;
+  }
 }
